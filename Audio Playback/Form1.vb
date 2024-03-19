535b7964c60722e833d52d6f650ed6198c6cc48a
Joseph Lumbley <joelumbley16@gmail.com>
2024-03-19 01:34:19 -0500

    Set Titlebar text to "Audio Playback"

--- Audio Playback/Form1.vb
+++ Audio Playback/Form1.vb
@@ -56,6 +56,8 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        Text = "Audio Playback"
+
         AppPath = Application.StartupPath
 
         CreateSoundFileFromResource()
