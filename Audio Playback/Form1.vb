0156bc8bc3581d06cf8b285c1b0c724a53879708
Joseph Lumbley <joelumbley16@gmail.com>
2024-03-19 13:05:54 -0500

    Changed titlebar text

--- Audio Playback/Form1.vb
+++ Audio Playback/Form1.vb
@@ -56,7 +56,7 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Text = "Audio Playback"
+        Text = "Audio Playback - Code with Joe"
 
         AppPath = Application.StartupPath
 
