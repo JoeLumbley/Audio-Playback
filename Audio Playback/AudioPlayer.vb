9473861a652d5582567ce7455193a67e370ee58d
Joseph Lumbley <j_lumbley@hotmail.com>
2026-09-22 00:45:40 -0500

    Add license header, author info, and file description
    Added MIT license header and author attribution to AudioPlayer.vb. Included a brief file description and appended a comment referencing the author's YouTube channel and Copilot as an AI assistant.

--- Audio Playback/AudioPlayer.vb
+++ Audio Playback/AudioPlayer.vb
@@ -1,4 +1,29 @@
-﻿Imports System.Runtime.InteropServices
+﻿' Audio Playback
+
+' Uses Windows MCI API (winmm.dll) for playback of multiple audio files simultaneously.
+
+' MIT License
+' Copyright(c) 2022 Joseph W. Lumbley
+
+' Permission is hereby granted, free of charge, to any person obtaining a copy
+' of this software and associated documentation files (the "Software"), to deal
+' in the Software without restriction, including without limitation the rights
+' to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+' copies of the Software, and to permit persons to whom the Software is
+' furnished to do so, subject to the following conditions:
+
+' The above copyright notice and this permission notice shall be included in all
+' copies or substantial portions of the Software.
+
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+' IMPLIED, INCLUDING BUT NOT LIMITED TO AND THE WARRANTIES OF MERCHANTABILITY,
+' FITNESS FOR A PARTICULAR PURPOSE A NONINFRINGEMENT. IN NO EVENT SHALL THE
+' AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+' LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+' OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+' SOFTWARE.
+
+Imports System.Runtime.InteropServices
 Imports System.Text
 Imports System.Threading.Tasks
 Imports System.IO
@@ -367,4 +392,9 @@ Public Class AudioPlayer
 End Class
 
 
+' Copilot is our AI assistant.
+
+
+' I also make coding videos on my YouTube channel: Code with Joe.
+' https://www.youtube.com/@codewithjoe6074
 
